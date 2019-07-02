8bb21e351e8bff4a33034ac2dd82d37c32ef2930
uurde <ugur@ugurdegirmenci.net>
2019-07-02 10:59:02 +0300

    txt to list exercise
    tried to read text file and print word by word

---
+++ Text2List/text2List.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+path = 'D:/Solutions/Python/uur.txt'
+
+wordList = []
+
+wordsFile = open(path, 'r')
+
+wordList = wordsFile.read().split(' ')
+
+for word in wordList: print(word)
+
+for i in range(len(wordList)):
+    print("{} {}".format(i + 1, wordList[i]))
+
